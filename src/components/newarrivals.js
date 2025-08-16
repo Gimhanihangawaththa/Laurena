78fddfe6449b0c9911069e573d37b450ed2cd9af
Gimhanihangawaththa <gimhani0327@gmail.com>
2025-08-16 13:23:52 +0530

    change newarrivals

--- src/components/newarrivals.js
+++ src/components/newarrivals.js
@@ -7,75 +7,51 @@ const newarrivalsData = [
   {
     id: 1,
     image: require('../assests/images/team1.jpg'),
-    fbLink: 'https://www.facebook.com',
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Diamond Drop Necklace',
-   
-   
+    description:'Single 0.5ct brilliant-cut diamond on an 18K white gold chain, GIA-certified for quality.',
   },
+  
   {
     id: 2,
     image: require('../assests/images/team2.jpg'),
-    fbLink: 'https://www.facebook.com',
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Celestial Star Ring',
-   
+    description:'South Sea pearls (10mm) mounted on sterling silver hooks, perfect for formal wear.',
   },
   {
     id: 3,
     image: require('../assests/images/team3.jpg'),
-    fbLink: 'https://www.facebook.com',
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Vintage Gold Band',
-   
+    description:'Sterling silver chain bracelet with five detachable charms, adjustable length.'
   },
   {
     id: 4,
     image: require('../assests/images/team4.jpg'),
-    fbLink: 'https://www.facebook.com',
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Emerald Royal Necklace',
-   
+    description:'Crafted from natural freshwater pearls, each hand-picked for luster, set on a 14K gold chain.'
   },
   {
     id: 5,
     image: require("../assests/images/team5.jpg"),
-    
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Sapphire Promise Bangle',
-    
+     description:'Intricate hand-carved 22K gold design, lightweight but sturdy.'
   },
   {
     id: 6,
-    image: require('../assests/images/team6.jpg'),
-    fbLink: 'https://www.facebook.com',
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
+    image: require('../assests/images/team6.jpg'), 
     name: 'Princess Cut Ring',
-   
+    description:'Medium-sized 14K gold hoops with secure hinged clasp, polished to a mirror finish.',
   },
   {
     id: 7,
     image: require('../assests/images/team7.jpg'),
-    fbLink: 'https://www.facebook.com',
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Sapphire Teardrop Earrings',
-  
+   description:'14K rose gold infinity band featuring 20 micro-pavé diamonds (total 0.3ct).'
   },
   {
     id: 8,
     image: require('../assests/images/team8.jpg'),
-   
-    twitterLink: 'https://www.twitter.com',
-    linkedinLink: 'https://www.linkedin.com',
     name: 'Minimalist Stud Earrings',
-   
+    description:' Smooth 22K solid gold band, 3mm width, perfect for everyday wear.'
   }
 ]
 
@@ -96,17 +72,15 @@ function AppNewarrivals() {
                     <div className='overlay'>
                       <div className='socials'>
                         <ul>
-                          <li><a href={ newarrivals.fbLink}><i className="fab fa-facebook-f"></i></a></li>
-                          <li><a href={ newarrivals.twitterLink}><i className="fab fa-twitter"></i></a></li>
-                          <li><a href={ newarrivals.linkedinLink}><i className="fab fa-linkedin-in"></i></a></li>
+                          
+                                           <p>{ newarrivals.description}</p>  
                         </ul>
                       </div>
                     </div>
                   </div>
                   <div className='content'>
                     <h3>{ newarrivals.name}</h3>
-                    <span className='designation'>{ newarrivals.designation}</span>
-                    <p>{ newarrivals.description}</p>
+                    
                   </div>
                 </Col>
               );
