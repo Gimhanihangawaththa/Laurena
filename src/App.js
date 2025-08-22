ed91593ef2ee23d723dc15c011afda92139c31ac
Gimhanihangawaththa <gimhani0327@gmail.com>
2025-08-22 09:01:25 +0530

    ttt

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ function App() {
           <AppHeader />
         </header>
 
-       
-          
               <main>
                 <AppHero />
                 <AppCollection />
--- src/components/header.js
+++ src/components/header.js
@@ -38,6 +38,7 @@ function AppHeader() {
               <Nav.Link href="#collection">Collection</Nav.Link>
               <Nav.Link href="#newarrivals">New Arrivals</Nav.Link>
               <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link onClick={() => setShowProfile(true)}>
